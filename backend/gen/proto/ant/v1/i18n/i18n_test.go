af2db93717447e7826fd3e0e096c3fa60fd29688
ant-dev <dev@antrader.io>
2026-06-19 05:57:07 +0000

    fix: use _ = m.String() to silence unused result vet errors

--- backend/gen/proto/ant/v1/i18n/i18n_test.go
+++ backend/gen/proto/ant/v1/i18n/i18n_test.go
@@ -29,206 +29,206 @@ func TestAllI18NTypes(t *testing.T) {
 	t.Run("BacktestParams", func(t *testing.T) {
 		m := &BacktestParamsI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Tuning", func(t *testing.T) {
 		m := &TuningI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Base", func(t *testing.T) {
 		m := &BaseI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 		m.ProtoReflect()
 	})
 	t.Run("Accounts", func(t *testing.T) {
 		m := &AccountsI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("AiCore", func(t *testing.T) {
 		m := &AiCoreI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Trading", func(t *testing.T) {
 		m := &TradingI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Dashboard", func(t *testing.T) {
 		m := &DashboardI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("AiSettings", func(t *testing.T) {
 		m := &AiSettingsI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("AiWizard", func(t *testing.T) {
 		m := &AiWizardI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("AiStore", func(t *testing.T) {
 		m := &AiStoreI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Analytics", func(t *testing.T) {
 		m := &AnalyticsI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Errors", func(t *testing.T) {
 		m := &ErrorsI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Admin", func(t *testing.T) {
 		m := &AdminI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("AiChat", func(t *testing.T) {
 		m := &AiChatI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("CodeEditor", func(t *testing.T) {
 		m := &CodeEditorI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Gen", func(t *testing.T) {
 		m := &GenI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("QuickTradeSection", func(t *testing.T) {
 		m := &QuickTradeSectionI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Ai", func(t *testing.T) {
 		m := &AiI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("AssetAnalysis", func(t *testing.T) {
 		m := &AssetAnalysisI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Asset", func(t *testing.T) {
 		m := &AssetI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Backtest", func(t *testing.T) {
 		m := &BacktestI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("BacktestRun", func(t *testing.T) {
 		m := &BacktestRunI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("ChartTools", func(t *testing.T) {
 		m := &ChartToolsI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("CodeAssist", func(t *testing.T) {
 		m := &CodeAssistI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("CodeQuality", func(t *testing.T) {
 		m := &CodeQualityI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("DefaultTemplates", func(t *testing.T) {
 		m := &DefaultTemplatesI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Experiment", func(t *testing.T) {
 		m := &ExperimentI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Library", func(t *testing.T) {
 		m := &LibraryI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Logs", func(t *testing.T) {
 		m := &LogsI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("MarketRegime", func(t *testing.T) {
 		m := &MarketRegimeI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Paper", func(t *testing.T) {
 		m := &PaperI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("ScheduleLogs", func(t *testing.T) {
 		m := &ScheduleLogsI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Schedules", func(t *testing.T) {
 		m := &SchedulesI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 	t.Run("Templates", func(t *testing.T) {
 		m := &TemplatesI18N{}
 		m.Reset()
-		m.String()
+		_ = m.String()
 		m.ProtoMessage()
 	})
 }
